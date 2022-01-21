c61a36cfd434f694ff89b7e3b65bd9cf7f2d4974
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2022-01-21 10:43:37 -0300

    api context change theme

--- components/template/Layout.tsx
+++ components/template/Layout.tsx
@@ -1,3 +1,4 @@
+import useAppData from "../../data/hook/UseAppData";
 import Content from "./Content";
 import SideBar from "./SideBar";
 import TopBar from "./TopBar";
@@ -9,8 +10,9 @@ interface LayoutProps {
 }
 
 export default function Layout(props: LayoutProps) {
+    const {tema} = useAppData()
     return (
-        <div className={`dark flex h-screen w-screen`}>
+        <div className={`${tema} flex h-screen w-screen`}>
             <SideBar />
             <div className={`flex flex-col w-full p-7 bg-gray-300 dark:bg-gray-800`}>
             <TopBar  titulo={props.titulo} subtitulo={props.subtitulo} />
--- pages/notificacoes.tsx
+++ pages/notificacoes.tsx
@@ -1,14 +1,14 @@
 import Layout from "../components/template/Layout";
 import useAppData from "../data/hook/UseAppData";
 
+
 export default function Notificacoes() {
-    const dados = useAppData()
+    const {alternarTema} = useAppData()
 
     return(
-        <Layout titulo="Notificações!" subtitulo="Aqui você irá receber as notificações">
-           
+        <Layout titulo="Notificações!" subtitulo="Aqui você irá receber as notificações">  
             <h3>Conteúdo notificações!!!</h3>
-            <h3>{dados.nome}</h3>
+            <button onClick={alternarTema}>Alternar Tema</button>
         </Layout>
     )
 }
