3ae044be8b8bb5842a4181349f4c8176fcf5fcf6
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2022-01-21 09:47:34 -0300

    Context API #1

--- pages/notificacoes.tsx
+++ pages/notificacoes.tsx
@@ -1,9 +1,14 @@
 import Layout from "../components/template/Layout";
+import useAppData from "../data/hook/UseAppData";
 
 export default function Notificacoes() {
+    const dados = useAppData()
+
     return(
         <Layout titulo="Notificações!" subtitulo="Aqui você irá receber as notificações">
+           
             <h3>Conteúdo notificações!!!</h3>
+            <h3>{dados.nome}</h3>
         </Layout>
     )
 }
