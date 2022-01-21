fe74ebb931c948111f0d1c186373c8b6bd99576a
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2022-01-21 16:03:20 -0300

    Styling authentication screen

--- components/auth/AthInput.tsx
+++ components/auth/AthInput.tsx
@@ -9,9 +9,15 @@ interface AuthInputProps {
 
 export default function AutInput(props: AuthInputProps) {
     return props.naoRenderizar ? null : (
-        <div className="flex flex-col">
+        <div className="flex flex-col mt-4">
             <label htmlFor="">{props.label}</label>
-            <input type={props.tipo ?? 'text'}
+            <input className={`
+                px-4 py-3 rounded-3
+                bg-gray-200 mt-2 border  focus:border-blue-500 
+                focus:bg-white 
+                focus:outline-none
+            `}
+            type={props.tipo ?? 'text'}
             value={props.valor}
             onChange={e => props.valoMudou?.(e.target.value)}
             required={props.obrigatorio}
